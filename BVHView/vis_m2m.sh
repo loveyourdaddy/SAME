bc7afcb9e424d70c456a07f90374aa8cc5db02ab
isjang08 <47512795+loveyourdaddy@users.noreply.github.com>
2026-08-28 09:57:22 +0900

    chores: add vis script

---
+++ BVHView/vis_m2m.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# bash /source/Inseo/SAME/BVHView/vis_m2m.sh
+SOFTWARE_GL=1 /source/Inseo/SAME/BVHView/render_bvhs_headless.sh /source/Inseo/Motion2Motion/260827/truebones_vt_groups_fold0_test
+SOFTWARE_GL=1 /source/Inseo/SAME/BVHView/render_bvhs_headless.sh /source/Inseo/Motion2Motion/260827/truebones_vt_groups_fold1_test
+SOFTWARE_GL=1 /source/Inseo/SAME/BVHView/render_bvhs_headless.sh /source/Inseo/Motion2Motion/260827/truebones_vt_groups_fold2_test
---
+++ BVHView/vis_same.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# bash /source/Inseo/SAME/BVHView/vis_same.sh
+SOFTWARE_GL=1 /source/Inseo/SAME/BVHView/render_bvhs_headless.sh /source/Inseo/SAME/result/260803_cfg_VT_fold0/test/tgt
+SOFTWARE_GL=1 /source/Inseo/SAME/BVHView/render_bvhs_headless.sh /source/Inseo/SAME/result/260803_cfg_VT_fold1/test/tgt
+SOFTWARE_GL=1 /source/Inseo/SAME/BVHView/render_bvhs_headless.sh /source/Inseo/SAME/result/260803_cfg_VT_fold2/test/tgt
